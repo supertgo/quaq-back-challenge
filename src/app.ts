0b98dbdec3a488921bca5784d1622ab74b04f0dc
supertgo <thiagoroberto2003@gmail.com>
2021-11-24 20:54:14 -0300

    Adding node documentation with Swagger

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,16 @@ import express from 'express'
 import config from 'config'
 import connect from './utils/connect'
 import routes from './routes'
-
+import swagger from 'swagger-ui-express'
+import swaggerDocs from './swagger.json'
 import deserializeUser from './middleware/ deserializeUser'
-
 
 
 const port = config.get<number>('port')
 
 const app = express();
-
 app.use(express.json())
+app.use('/api-docs', swagger.serve, swagger.setup(swaggerDocs))
 app.use(deserializeUser)
 
 app.listen(port, async() => {
